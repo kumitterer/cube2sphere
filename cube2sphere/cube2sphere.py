3a67a238bacecf23a57ea40565eebf47aba6a7f4
Kumi <git@kumi.email>
2024-12-13 09:15:37 +0100

    refactor: Refactors cube to sphere conversion process
    Introduces a Cube2Sphere class to encapsulate parameters and methods for improved modularity and readability.
    
    Refactors argument parsing and validation logic within main(), reducing redundancy and enhancing error handling.
    
    Extends .gitignore to include 'venv/' for better virtual environment management.
    
    This restructuring enhances maintainability and promotes better object-oriented design.

--- cube2sphere/cube2sphere.py
+++ cube2sphere/cube2sphere.py
@@ -1,75 +1,241 @@
-#!/usr/bin/env python
-
-__author__ = 'Xyene'
-
 import argparse
 import os
 import sys
 import subprocess
 import math
 from .version import __version__
+from typing import Optional
+
+
+class Cube2Sphere:
+    def __init__(
+        self,
+        front: os.PathLike,
+        back: os.PathLike,
+        left: os.PathLike,
+        right: os.PathLike,
+        top: os.PathLike,
+        bottom: os.PathLike,
+        resolution: tuple[int, int] = (1024, 512),
+        rotation: tuple[int, int, int] = (0, 0, 0),
+        output: os.PathLike = "out",
+        fmt: str = "TGA",
+        blender_path: os.PathLike = "blender",
+        threads: Optional[int] = None,
+        verbose: bool = False,
+    ):
+        """Initializes the Cube2Sphere object.
+
+        Args:
+            front (os.PathLike): Path to the front face of the cube.
+            back (os.PathLike): Path to the back face of the cube.
+            left (os.PathLike): Path to the left face of the cube.
+            right (os.PathLike): Path to the right face of the cube.
+            top (os.PathLike): Path to the top face of the cube.
+            bottom (os.PathLike): Path to the bottom face of the cube.
+            resolution (tuple[int, int], optional): Resolution of the output image. Defaults to (1024, 512).
+            rotation (tuple[int, int, int], optional): Rotation to apply before rendering the image. Defaults to (0, 0, 0).
+            output (os.PathLike, optional): Path to save the output image. Defaults to "out".
+            fmt (str, optional): Format of the output image. Defaults to "TGA".
+            blender_path (os.PathLike, optional): Path to the Blender executable. Defaults to "blender".
+            threads (Optional[int], optional): Number of threads to use when rendering. Defaults to None.
+            verbose (bool, optional): Enable verbose logging. Defaults to False.
+        """
+
+        self.faces = {
+            "front": front,
+            "back": back,
+            "left": left,
+            "right": right,
+            "top": top,
+            "bottom": bottom,
+        }
+        self.resolution = resolution
+        self.rotation = [math.radians(x) for x in rotation]
+        self.output = (
+            output if os.path.isabs(output) else os.path.join(os.getcwd(), output)
+        )
+        self.format = fmt
+        self.blender_path = blender_path
+        self.threads = threads
+        self.verbose = verbose
+
+    def validate(self) -> None:
+        """Validates the input arguments.
+
+        Raises:
+            ValueError: If any of the cube faces are not valid files.
+            ValueError: If the number of threads is out of range.
+        """
+
+        if not all(os.path.isfile(face) for face in self.faces.values()):
+            raise ValueError("All cube faces must be valid files")
+
+        if self.threads and (self.threads < 1 or self.threads > 64):
+            raise ValueError("Too many threads specified (range is 1-64)")
+
+    def convert(self) -> None:
+        """Converts the cube faces to a sphere.
+
+        Raises:
+            RuntimeError: If the Blender executable cannot be spawned.
+            RuntimeError: If Blender exits with a non-zero return code.
+        """
+        self.validate()
+        out = open(os.devnull, "w") if not self.verbose else None
+        faces_paths = [self.absolute_path(face) for face in self.faces.values()]
+
+        try:
+            process = subprocess.Popen(
+                [
+                    self.blender_path,
+                    "-E",
+                    "CYCLES",
+                    "--background",
+                    "-noaudio",
+                    "-b",
+                    os.path.join(
+                        os.path.dirname(os.path.realpath(__file__)), "projector.blend"
+                    ),
+                    "-o",
+                    self.output,
+                    "-F",
+                    self.format,
+                    "-x",
+                    "1",
+                    "-P",
+                    os.path.join(
+                        os.path.dirname(os.path.realpath(__file__)), "blender_init.py"
+                    ),
+                ]
+                + (["-t", str(self.threads)] if self.threads else [])
+                + [
+                    "--",
+                    *faces_paths,
+                    str(self.resolution[0]),
+                    str(self.resolution[1]),
+                    str(self.rotation[0]),
+                    str(self.rotation[1]),
+                    str(self.rotation[2]),
+                ],
+                stderr=out,
+                stdout=out,
+            )
+        except Exception as e:
+            raise RuntimeError("Error spawning blender executable") from e
+        else:
+            process.wait()
+            if process.returncode:
+                raise RuntimeError(
+                    f"Blender exited with error code {process.returncode}"
+                )
+
+    def absolute_path(self, path: os.PathLike) -> os.PathLike:
+        """Helper function to get the absolute path of a file.
+
+        Args:
+            path (os.PathLike): Path to the file.
+
+        Returns:
+            os.PathLike: Absolute path of the file.
+        """
+        return path if os.path.isabs(path) else os.path.join(os.getcwd(), path)
 
 
 def main():
-    _parser = argparse.ArgumentParser(prog='cube2sphere', description='''
+    """Main function to parse command line arguments and run the conversion."""
+
+    parser = argparse.ArgumentParser(
+        prog="cube2sphere",
+        description="""
         Maps 6 cube (cubemap, skybox) faces into an equirectangular (cylindrical projection, skysphere) map.
-    ''')
-    for f in ['front', 'back', 'left', 'right', 'top', 'bottom']:
-        _parser.add_argument(f, type=str, metavar='<%s>' % f, help='source %s cube face filename' % f)
-    _parser.add_argument('-v', '--version', action='version', version=__version__)
-    _parser.add_argument('-r', '--resolution', type=int, nargs=2, default=[1024, 512], metavar=('<width>', '<height>'),
-                         help='resolution for rendered map (defaults to 1024x512)')
-    _parser.add_argument('-R', '--rotation', type=int, nargs=3, default=[0, 0, 0], metavar=('<rx>', '<ry>', '<rz>'),
-                         help="rotation in degrees to apply before rendering map (z is up)")
-    _parser.add_argument('-o', '--output', type=str, default='out', metavar='<path>',
-                         help='filename for rendered map (defaults to "out")')
-    _parser.add_argument('-f', '--format', type=str, default='TGA', metavar='<name>',
-                         help='format to use when saving map, i.e. "PNG" or "TGA"')
-    _parser.add_argument('-b', '--blender-path', type=str, default='blender', metavar='<path>',
-                         help='filename of the Blender executable (defaults to "blender")')
-    _parser.add_argument('-t', '--threads', type=int, default=None, metavar='<count>',
-                         help='number of threads to use when rendering (1-64)')
-    _parser.add_argument('-V', '--verbose', action='store_true',
-                         help='enable verbose logging')
-    _args = _parser.parse_args()
+    """,
+    )
 
-    rotations = [math.radians(x) for x in _args.rotation]
+    for f in ["front", "back", "left", "right", "top", "bottom"]:
+        parser.add_argument(
+            f, type=str, metavar="<%s>" % f, help=f"source {f} cube face filename"
+        )
 
-    if _args.threads and _args.threads not in list(range(1, 65)):
-        _parser.print_usage()
-        print('cube2sphere: error: too many threads specified (range is 1-64)')
-        sys.exit(1)
+    parser.add_argument("-v", "--version", action="version", version=__version__)
+    parser.add_argument(
+        "-r",
+        "--resolution",
+        type=int,
+        nargs=2,
+        default=[1024, 512],
+        metavar=("<width>", "<height>"),
+        help="resolution for rendered map (defaults to 1024x512)",
+    )
+    parser.add_argument(
+        "-R",
+        "--rotation",
+        type=int,
+        nargs=3,
+        default=[0, 0, 0],
+        metavar=("<rx>", "<ry>", "<rz>"),
+        help="rotation in degrees to apply before rendering map (z is up)",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="out",
+        metavar="<path>",
+        help='filename for rendered map (defaults to "out")',
+    )
+    parser.add_argument(
+        "-f",
+        "--format",
+        type=str,
+        default="TGA",
+        metavar="<name>",
+        help='format to use when saving map, i.e. "PNG" or "TGA"',
+    )
+    parser.add_argument(
+        "-b",
+        "--blender-path",
+        type=str,
+        default="blender",
+        metavar="<path>",
+        help='filename of the Blender executable (defaults to "blender")',
+    )
+    parser.add_argument(
+        "-t",
+        "--threads",
+        type=int,
+        default=None,
+        metavar="<count>",
+        help="number of threads to use when rendering (1-64)",
+    )
+    parser.add_argument(
+        "-V", "--verbose", action="store_true", help="enable verbose logging"
+    )
 
-    output = _args.output
-    output = output if os.path.isabs(output) else os.path.join(os.getcwd(), output)
-
-    out = open(os.devnull, 'w') if not _args.verbose else None
-
-    faces = [_args.front, _args.back, _args.left, _args.right, _args.top, _args.bottom]
-    for i in range(len(faces)):
-        face = faces[i]
-        face = faces[i] if os.path.isabs(face) else os.path.join(os.getcwd(), face)
-        faces[i] = face
+    args = parser.parse_args()
 
     try:
-        process = subprocess.Popen(
-            [_args.blender_path, '-E', 'CYCLES', '--background', '-noaudio',
-             '-b', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'projector.blend'),
-             '-o', output, '-F', _args.format, '-x', '1',
-             '-P', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'blender_init.py')]
-            + (['-t', str(_args.threads)] if _args.threads else [])
-            + ['--', faces[0], faces[1], faces[2], faces[3], faces[4], faces[5],
-               str(_args.resolution[0]), str(_args.resolution[1]),
-               str(rotations[0]), str(rotations[1]), str(rotations[2])],
-            stderr=out, stdout=out)
-    except:
-        print('error spawning blender (%s) executable' % _args.blender_path)
-        import traceback
-
-        traceback.print_exc()
+        cube2sphere = Cube2Sphere(
+            front=args.front,
+            back=args.back,
+            left=args.left,
+            right=args.right,
+            top=args.top,
+            bottom=args.bottom,
+            resolution=args.resolution,
+            rotation=args.rotation,
+            output=args.output,
+            fmt=args.format,
+            blender_path=args.blender_path,
+            threads=args.threads,
+            verbose=args.verbose,
+        )
+        cube2sphere.convert()
+    except Exception as e:
+        parser.print_usage()
+        print(f"cube2sphere: error: {e}")
         sys.exit(1)
-    else:
-        process.wait()
-        if process.returncode:
-            print('blender exited with error code %d' % process.returncode)
-            sys.exit(process.returncode)
+
+
+if __name__ == "__main__":
+    main()
